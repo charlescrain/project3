98db5bcdbe115b3ac95a9ee5d331e0f951e4f48e
charlescrain <charlescrain@umail.ucsb.edu>
2014-03-02 18:47:39 -0800

    Changes to input and New Heap Class

---
+++ Heap.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "Node.h"
+#include "LinkedList.h"
+using namespace std;
+
+class Heap {
+	private:
+		Relation *array;
+		int size;
+
+	public:
+		Heap(Relation *);		//constructor
+		
+
+};
